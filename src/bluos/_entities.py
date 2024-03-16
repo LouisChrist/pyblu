1937d457776c04da7349f55ef54942c3c7aa2020
Louis Christ <mail@louischrist.de>
2024-03-16 22:21:48 +0100

    add doc strings to entities

--- src/bluos/_entities.py
+++ src/bluos/_entities.py
@@ -4,21 +4,34 @@ from dataclasses import dataclass
 @dataclass
 class Status:
     etag: str
+    """Cursor for long polling requests"""
     state: str
+    """Playback state"""
 
     album: str
+    """Album name"""
     artist: str
+    """Artist name"""
     name: str
+    """Track name"""
     image: str
+    """URL of the album art"""
 
     volume: int
+    """Volume level with a range of 0-100"""
     mute: bool
+    """Mute status"""
     seconds: int
+    """Current playback position in seconds"""
     total_seconds: float
+    """Total track length in seconds"""
 
 
 @dataclass
 class Volume:
     volume: int
+    """Volume level with a range of 0-100"""
     db: float
+    """Volume level in dB"""
     mute: bool
+    """Mute status"""
