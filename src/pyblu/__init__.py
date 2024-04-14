20e7f4d717a987345645446cb067be1067108753
Louis Christ <mail@louischrist.de>
2024-04-14 23:03:27 +0200

    add __all__

--- src/pyblu/__init__.py
+++ src/pyblu/__init__.py
@@ -1,4 +1,6 @@
 """A Python library for controlling BluOS players."""
 
 from ._player import Player
-from ._entities import Status, Volume, SyncStatus, PairedPlayer
+from ._entities import Status, Volume, SyncStatus, PairedPlayer, PlayQueue, Preset, Input
+
+__all__ = ["Player", "Status", "Volume", "SyncStatus", "PairedPlayer", "PlayQueue", "Preset", "Input"]
